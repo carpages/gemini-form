331ccae303cbaa8a3a7b4468cfa447f584f379ab
Jeff Andrews <jeff.andrews@carpages.ca>
2018-03-29 16:41:17 -0400

    Update rudimentary integration tests.

--- test/gemini-form.edge.js
+++ test/gemini-form.edge.js
@@ -2,31 +2,28 @@
 requirejs.config({
   baseUrl: '../',
   paths  : {
-    'underscore'           : 'bower_components/underscore/underscore',
-    'jquery'               : 'bower_components/jquery/dist/jquery',
-    'handlebars'           : 'bower_components/handlebars/handlebars.runtime',
+    underscore             : 'bower_components/underscore/underscore',
+    jquery                 : 'bower_components/jquery/dist/jquery',
+    handlebars             : 'bower_components/handlebars/handlebars.runtime',
     'jquery.boiler'        : 'bower_components/jquery-boiler/jquery.boiler',
     'jquery.mockjax'       : 'bower_components/jquery-mockjax/src/jquery.mockjax',
     'gemini.support'       : 'bower_components/gemini-support/gemini.support',
-    'gemini'               : 'bower_components/gemini-loader/gemini',
+    gemini                 : 'bower_components/gemini-loader/gemini',
     'gemini.form.templates': 'templates'
   }
 });
 
-require([
-  'gemini',
-  'gemini.form',
-  'jquery.mockjax'
-], function( G ) {
+require([ 'gemini', 'gemini.form', 'jquery.mockjax' ], function( G ) {
   $.mockjax({
     url         : '/edge/test',
     responseText: {
-      status : 'success',
-      fortune: 'Are you a mock turtle?'
+      status : 'error',
+      message: 'Are you a mock turtle?'
     }
   });
 
   G( '#js-ajax-form' ).form({
+    formAlertTarget : '.form__errors',
     inputAlertTarget: '.field__errors',
     inputAlertParent: '.field'
   });
